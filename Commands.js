41f315c7a0a312841f21ae1cda68979c1d3dc395
AAntonSB <anton.s.bergqvist@gmail.com>
2020-09-28 14:15:35 +0200

    prompt function

--- Commands.js
+++ Commands.js
@@ -1,1 +1,11 @@
-module.exports = class Commands { }
+const prompts = require("prompts");
+
+module.exports = class Commands {
+  prompt = async (message) => {
+    return await prompts({
+      type: "text",
+      name: "value",
+      message: message,
+    });
+  };
+};
