9c00270e317ae0691b1290260ae3c435ea8cbf35
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 18:00:02 +0200

    Added RoomModuleFactory

--- RoomModuleFactory.js
+++ RoomModuleFactory.js
@@ -1,11 +1,14 @@
-class Exit {
+let registeredRoomModules = {};
+
+registeredRoomModules["exit"] = class Exit {
   constructor(props) {
     this.direction = props.direction;
     this.node = props.node;
     this.distance = props.distance;
   }
 };
-class Item {
+
+registeredRoomModules["item"] = class Item {
   constructor(props) {
     this.id = props.id;
     this.name = props.name;
@@ -17,7 +20,7 @@ class Item {
   }
 };
 
-class Npc {
+registeredRoomModules["npc"] = class Npc {
   constructor(props) {
     this.id = props.id;
     this.name = props.name;
@@ -25,5 +28,11 @@ class Npc {
     this.actOptions = props.actOptions || [];
     this.stats = props.stats || {};
     this.friendly = props.friendly || false;
+  }
+};
+//The abstract factory class, creates room
+module.exports = class RoomModuleFactory {
+  constructor(type, props) {
+    return new registeredRoomModules[type](props);
   }
 };
