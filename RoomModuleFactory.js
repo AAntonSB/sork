6eb1d22f2879e03200b911da2c1a00aa5546e949
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 17:58:52 +0200

    Added item class

--- RoomModuleFactory.js
+++ RoomModuleFactory.js
@@ -5,5 +5,16 @@ class Exit {
     this.distance = props.distance;
   }
 };
+class Item {
+  constructor(props) {
+    this.id = props.id;
+    this.name = props.name;
+    this.description = props.description;
+    this.triggers = props.triggers || [];
+    this.equipable = props.equipable || false;
+    this.attackDescription = props.attackDescription || "";
+    this.damage = props.damage || null;
+  }
+};
 
 
