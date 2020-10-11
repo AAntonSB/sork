20915fce8990bb7eca91d494d6f47533318d2814
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 18:03:26 +0200

    Create rooms skeleton

--- Map.js
+++ Map.js
@@ -1,5 +1,20 @@
+const Room = require("./Room.js");
+const RoomModuleFactory = require("./RoomModuleFactory.js");
+
 module.exports = class Map {
-  constructor(blueprint) {
-    this.blueprint = blueprint;
+  constructor(rooms) {
+    this.rooms = rooms;
   }
+
+  //This function creates all the <Room>s and all the modules it contains: <Exit>, <Item>, <Npc>
+  createRooms = () => {
+    this.rooms = this.rooms.map((roomDetails) => {
+      return new Room(roomDetails.id, {
+        name: roomDetails.name,
+        first: roomDetails.first,
+        description: roomDetails.description,
+        items: roomDetails.items
+      });
+    });
+  };
 };
