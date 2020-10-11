ab9f23770e144d4bb3aa16dcde4d9c3cf18a2f6b
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 18:11:09 +0200

    Added abstract action class

---
+++ Action.js
@@ -0,0 +1,15 @@
+//Chain of command pattern
+class Action {
+  constructor() {
+    if (this.constructor === Action) {
+      throw new Error("AnAbstractClass can not be instantiated");
+    }
+  }
+
+  setNextChain() {
+    throw new Error("setNextChain() is not implemented");
+  }
+  respondToInput() {
+    throw new Error("respondToInput() is not implemented");
+  }
+}
