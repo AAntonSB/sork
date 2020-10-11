3abcc1b166a22c7d954b00f781864c923e57540f
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 18:18:50 +0200

    Implemented attemptToChangeRooms function

--- World.js
+++ World.js
@@ -9,4 +9,19 @@ module.exports = class World {
 
   description = () => {};
   options = () => {};
+
+  attemptToChangeRooms(direction) {
+    const e = this.currRoom.exits.find((exit) => exit.direction === direction);
+    if (e) {
+      console.log("You head off to the " + direction);
+      this.currRoom = this.map.rooms.filter((room) => {
+        return e.node === room.id;
+      })[0];
+      console.clear();
+      this.descriptionLogged = false;
+    } else {
+      console.log("There is no path to the " + direction);
+    }
+  }
+
 };
