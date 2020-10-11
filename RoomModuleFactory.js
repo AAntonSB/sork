b3b83b4c0f8887caf35aa9bce178d8255c6fbe9f
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 22:42:48 +0200

    added bridge-pattern

--- RoomModuleFactory.js
+++ RoomModuleFactory.js
@@ -1,3 +1,5 @@
+// https://www.digitalocean.com/community/tutorials/js-factory-pattern
+
 let registeredRoomModules = {};
 
 registeredRoomModules["exit"] = class Exit {
@@ -8,15 +10,52 @@ registeredRoomModules["exit"] = class Exit {
   }
 };
 
-registeredRoomModules["item"] = class Item {
+class Item {
   constructor(props) {
     this.id = props.id;
     this.name = props.name;
     this.description = props.description;
     this.triggers = props.triggers || [];
+  }
+}
+
+//Bridge factory
+class Weapon extends Item {
+  constructor(props) {
+    super(props);
     this.equipable = props.equipable || false;
     this.attackDescription = props.attackDescription || "";
     this.damage = props.damage || null;
+  }
+}
+
+class Tool extends Item {
+  constructor(props) {
+    super(props);
+  }
+}
+
+class StaticItem extends Item {
+  constructor(props) {
+    super(props);
+  }
+}
+
+registeredRoomModules["item"] = class ItemFactory {
+  static createItem(props) {
+    if (props.id === "sword") {
+      return new Weapon(props);
+    }
+
+    if (props.id === "torch") {
+      return new Tool(props);
+    }
+
+    if (props.static) {
+      return new StaticItem(props);
+    }
+    console.log(props);
+    throw new Error("uknown item");
   }
 };
 
@@ -32,7 +71,13 @@ registeredRoomModules["npc"] = class Npc {
 };
 //The abstract factory class, creates room
 module.exports = class RoomModuleFactory {
-  constructor(type, props) {
-    return new registeredRoomModules[type](props);
+  static createExit(props) {
+    return new registeredRoomModules["exit"](props);
+  }
+  static createItem(props) {
+    return registeredRoomModules["item"].createItem(props);
+  }
+  static createNpc(props) {
+    return new registeredRoomModules["npc"](props);
   }
 };
