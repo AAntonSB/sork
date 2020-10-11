449966d0b5722d1d50d0acc40033b7af566b7ce5
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 17:52:29 +0200

    Extended Character class to fit game structure and match npc

--- Character.js
+++ Character.js
@@ -1,5 +1,52 @@
 module.exports = class Character {
-  constructor(name) {
+  constructor(name, { hp }) {
     this.name = name;
+    this.stats = { hp };
+    this.inventory = [];
+    this.effects = [];
+    this.active = {};
+  }
+
+  //#region INVENTORY
+
+  displayInventory() {
+    return this.inventory.map((e) => {
+      return e.name;
+    });
+  }
+
+  addToInventory(item) {
+    this.inventory.push(item);
+  }
+
+  removeFromInventory(item) {
+    const itemIndex = this.inventory.indexOf((element) => element === item);
+    this.inventory.splice(itemIndex, 1);
+  }
+
+  //#endregion
+
+  //#region GETTERS AND SETTERS
+
+  getStats() {
+    return this.stats;
+  }
+
+  setStats(value) {
+    this.stats = value;
+  }
+
+  setActive(value) {
+    this.active = value;
+  }
+
+  getActive() {
+    return this.active;
+  }
+
+  //#endregion
+
+  addEffect(effect) {
+    this.effects.push(effect);
   }
 };
