bc3d2bbc869b26ee588a2f0d67d1edf574ae6582
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 16:41:46 +0200

    Extended the room class to fit the game structure

--- Room.js
+++ Room.js
@@ -1,1 +1,11 @@
-module.exports = class Room { }
+//acts as a javascript builder
+module.exports = class Room {
+  constructor(id, { name, exits, description, items, npcs } = {}) {
+    this.id = id;
+    this.name = name;
+    this.exits = exits;
+    this.description = description;
+    this.items = items;
+    this.npcs = npcs;
+  }
+};
