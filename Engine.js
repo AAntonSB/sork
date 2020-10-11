b52f9859b2dded46af4491f0ef5103240e71125c
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-11 18:07:16 +0200

    Engine done function

--- Engine.js
+++ Engine.js
@@ -6,10 +6,15 @@ module.exports = class Engine {
     if (Engine.exists) {
       return Engine.instance;
     }
+    this.game = game;
 
     Engine.instance = this;
     Engine.exists = true;
     return this;
   }
+  //returns false once the player dies (Planed to be expanded on so the gamemaker can set the conditions)
+  done = (world) => {
+    return world.player.stats.hp > 0;
+  };
   }
 };
